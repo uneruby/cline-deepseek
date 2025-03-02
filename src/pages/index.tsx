8c148e5d40cd67a4a2294b773d316e02e8422b35
uneruby <uneshota@team-lab.com>
2025-03-03 03:16:03 +0900

    chore: ボタンを作らせてみた

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,15 @@
 import styles from '@/styles/Home.module.scss';
+import Button from '@/components/Button';
 
 export default function Home() {
+  const handleClick = () => {
+    console.log('Button clicked!');
+  };
+
   return (
-    <>
-      <div className={styles.gomi}>
-        <p>gomi</p>
-      </div>
-    </>
+    <div className={styles.container}>
+      <h1>Button Component Example</h1>
+      <Button onClick={handleClick}>Click Me</Button>
+    </div>
   );
 }
