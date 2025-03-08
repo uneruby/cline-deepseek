ec602c758afecd6a1caec899075b915300b54944
uneruby <uneshota@team-lab.com>
2025-03-08 15:14:47 +0900

    :sparkles: pageにアコーディオン表示

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import styles from '@/styles/Home.module.scss';
 import Button from '@/components/button/BaseButton';
+import { AccordionBanner } from '@/components/banner/AccordionBanner';
 
 export default function Home() {
   const handleClick = () => {
@@ -9,6 +10,10 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <h1>Button Component Example</h1>
+      <AccordionBanner
+        mainText="Main Banner Text"
+        subText="Additional information that appears when expanded"
+      />
       <Button onClick={handleClick}>Click Me</Button>
     </div>
   );
